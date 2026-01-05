8936740517375c484e2f637298cdd2f3dfeb3aae
Xibsy <pro45951@gmail.com>
2026-01-05 08:29:17 +0500

    a

---
+++ constants.py
@@ -0,0 +1,15 @@
+import arcade.math
+from mathematics.vector import Vector2Int, Vector2
+
+TITLE = 'ROOT-EXECUTION'
+SCREEN_SHAPE = Vector2Int(1080, 720)
+
+SHAPE = Vector2(40, 60)
+
+SHOOT_FREQUENCY = 3
+SHOOT_DELTA = SHAPE * .5
+
+MAX_SPEED = Vector2(10, 10)
+ACCELERATION = 300
+
+PLAYER_COLOR = arcade.color.GRAPE
