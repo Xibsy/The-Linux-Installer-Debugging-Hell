4c8c10b00c5e61b47c11281f8584912007a731f9
Xibsy <pro45951@gmail.com>
2026-01-09 14:05:12 +0500

    a game_engine bruh

---
+++ game_engine.py
@@ -0,0 +1,36 @@
+import arcade
+
+from mathematics.vector import Vector2Int, Vector2
+from draw import Draw
+import protocols as proto
+from player_inputer import PlayerInputer
+
+
+class GameEngine(arcade.Window):
+    def __init__(self,
+                 title: str,
+                 screen_shape: Vector2Int,
+                 draw: Draw,
+                 player: proto.Player) -> None:
+        super().__init__(screen_shape.x, screen_shape.y, title, vsync=True)
+        self.background_color = arcade.color.LIME_GREEN
+        self._draw = draw
+        self._player = player
+
+        self._player_inputer = PlayerInputer()
+
+    @property
+    def player_inputer(self) -> PlayerInputer:
+        return self._player_inputer
+
+    def on_key_press(self, symbol: int, modifiers: int) -> None:
+        self._player_inputer.pressed_keys.add(symbol)
+        self._player_inputer.keyboard_state.invoke(self._player_inputer.pressed_keys)
+
+    def on_fixed_update(self, delta_time: float) -> None:
+        self._player.update(delta_time)
+
+    def on_draw(self) -> None:
+        self.clear()
+        self._draw.player(self._player)
+
